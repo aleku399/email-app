8291cd46b7a6554691180687a2c927c6d4f1f9da
alex <alex@alexs-MacBook-Pro.local>
2024-10-21 12:52:59 +0300

    Fix cors Safari

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -2,9 +2,14 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 // CORS middleware to set headers
-const corsMiddleware = (res: NextResponse) => {
-  // res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Origin', 'https://www.henkmininglogistics.com');
+const corsMiddleware = (req: Request, res: NextResponse) => {
+  const allowedOrigin = 'https://www.henkmininglogistics.com';
+  const origin = req.headers.get('origin');
+  
+  if (origin === allowedOrigin) {
+    res.headers.set('Access-Control-Allow-Origin', allowedOrigin);
+  }
+  
   res.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
   res.headers.set('Vary', 'Origin');
@@ -12,11 +17,10 @@ const corsMiddleware = (res: NextResponse) => {
 
 // Handle the POST request for sending emails
 export async function POST(req: Request) {
-  // Create the initial response object
   const response = NextResponse.json({}, { status: 200 });
 
   // Apply CORS headers
-  corsMiddleware(response);
+  corsMiddleware(req, response);
 
   try {
     const { name, lastName, organisation, email, phoneNumber, message, country, subject } = await req.json();
@@ -48,21 +52,19 @@ ${message}`,
 
     await transporter.sendMail(mailOptions);
 
-    // Return success response
     return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error sending email:', error);
-    // Return error response
     return NextResponse.json({ message: 'Failed to send email' }, { status: 500 });
   }
 }
 
 // Handle the preflight OPTIONS request for CORS
-export function OPTIONS() {
+export function OPTIONS(req: Request) {
   const response = new NextResponse(null, { status: 204 });
   
   // Apply CORS headers
-  corsMiddleware(response); 
+  corsMiddleware(req, response); 
   
   return response;
 }
